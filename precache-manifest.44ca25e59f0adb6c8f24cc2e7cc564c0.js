a4b637e840a1e95e4285abacc13c86a82a021c3f
Guille Cirer <guille.mca@hotmail.com>
2019-03-25 18:48:59 +0100

    Updates

---
+++ precache-manifest.44ca25e59f0adb6c8f24cc2e7cc564c0.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = [
+  {
+    "revision": "da408238128b876cbda6424391f1566f",
+    "url": "/Loyalty-app-react/static/media/fa-brands-400.da408238.woff"
+  },
+  {
+    "revision": "eee20e092c208b98389b",
+    "url": "/Loyalty-app-react/static/css/main.34c9739c.chunk.css"
+  },
+  {
+    "revision": "edd87634275c6a47782f",
+    "url": "/Loyalty-app-react/static/js/runtime~main.87886bcb.js"
+  },
+  {
+    "revision": "de1d242d8acb26ec43c0d071fe78e72d",
+    "url": "/Loyalty-app-react/static/media/fa-solid-900.de1d242d.svg"
+  },
+  {
+    "revision": "602b1714f69930542ea5",
+    "url": "/Loyalty-app-react/static/js/2.d636e3cd.chunk.js"
+  },
+  {
+    "revision": "8d9ab84bfe87a3f77112a6698cf639fb",
+    "url": "/Loyalty-app-react/static/media/fa-regular-400.8d9ab84b.woff2"
+  },
+  {
+    "revision": "dad90637f797356bbc70d2664832e0b6",
+    "url": "/Loyalty-app-react/static/media/fa-regular-400.dad90637.woff"
+  },
+  {
+    "revision": "8d220c793e2612bd131ed8522c54669f",
+    "url": "/Loyalty-app-react/static/media/fa-regular-400.8d220c79.ttf"
+  },
+  {
+    "revision": "e6c93cb47e716b579264a5fdfbf7e84d",
+    "url": "/Loyalty-app-react/static/media/fa-regular-400.e6c93cb4.eot"
+  },
+  {
+    "revision": "a4d31128b633bc0b1cc1f18a34fb3851",
+    "url": "/Loyalty-app-react/static/media/Material-Design-Iconic-Font.a4d31128.woff2"
+  },
+  {
+    "revision": "d2a55d331bdd1a7ea97a8a1fbb3c569c",
+    "url": "/Loyalty-app-react/static/media/Material-Design-Iconic-Font.d2a55d33.woff"
+  },
+  {
+    "revision": "6814d0e8136d34e313623eb7129d538e",
+    "url": "/Loyalty-app-react/static/media/fa-brands-400.6814d0e8.woff2"
+  },
+  {
+    "revision": "b75b4bfe0d58faeced5006c785eaae23",
+    "url": "/Loyalty-app-react/static/media/fa-solid-900.b75b4bfe.woff2"
+  },
+  {
+    "revision": "2c159d0d05473040b53ec79df8797d32",
+    "url": "/Loyalty-app-react/static/media/ionicons.2c159d0d.woff"
+  },
+  {
+    "revision": "eee20e092c208b98389b",
+    "url": "/Loyalty-app-react/static/js/main.78a1844c.chunk.js"
+  },
+  {
+    "revision": "2d0415fa29ea596b7a02c78eddeede20",
+    "url": "/Loyalty-app-react/static/media/fa-solid-900.2d0415fa.woff"
+  },
+  {
+    "revision": "b351bd62abcd96e924d9f44a3da169a7",
+    "url": "/Loyalty-app-react/static/media/Material-Design-Iconic-Font.b351bd62.ttf"
+  },
+  {
+    "revision": "e8019d507e8cb51d169ab4f94a0cda12",
+    "url": "/Loyalty-app-react/static/media/fa-brands-400.e8019d50.eot"
+  },
+  {
+    "revision": "fdf44bc43e8fa2358bbb7d9165d78455",
+    "url": "/Loyalty-app-react/static/media/fa-brands-400.fdf44bc4.ttf"
+  },
+  {
+    "revision": "19e65b89cee273a249fba4c09b951b74",
+    "url": "/Loyalty-app-react/static/media/ionicons.19e65b89.eot"
+  },
+  {
+    "revision": "ba2a91dc95e6cfdc4b2a186a7ba83e29",
+    "url": "/Loyalty-app-react/static/media/fa-regular-400.ba2a91dc.svg"
+  },
+  {
+    "revision": "ea363ed422723673917901680be9b37c",
+    "url": "/Loyalty-app-react/static/media/fa-solid-900.ea363ed4.eot"
+  },
+  {
+    "revision": "132e9759d93e4eefd7cdde0d7a322991",
+    "url": "/Loyalty-app-react/static/media/fa-solid-900.132e9759.ttf"
+  },
+  {
+    "revision": "dd4781d1acc57ba4c4808d1b44301201",
+    "url": "/Loyalty-app-react/static/media/ionicons.dd4781d1.ttf"
+  },
+  {
+    "revision": "aff28a207631f39ee0272d5cdde43ee7",
+    "url": "/Loyalty-app-react/static/media/ionicons.aff28a20.svg"
+  },
+  {
+    "revision": "83e6c29fb363b2f0ea6cc18fefff729c",
+    "url": "/Loyalty-app-react/static/media/fa-brands-400.83e6c29f.svg"
+  },
+  {
+    "revision": "602b1714f69930542ea5",
+    "url": "/Loyalty-app-react/static/css/2.35f2d37c.chunk.css"
+  },
+  {
+    "revision": "eb5fa0dc59ba0d7f5aa715b75188682e",
+    "url": "/Loyalty-app-react/index.html"
+  }
+];
